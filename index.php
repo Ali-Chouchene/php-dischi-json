9089cd3ed5b6f79830109eff45d4b4c0a4bf7fda
Ali-Chouchene <choucheneali9801@gmail.com>
2023-02-09 18:04:54 +0100

    done without bonus

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+$source_url = __DIR__ . '/data.json';
+
+$data_json = file_get_contents($source_url);
+
+$discs = json_decode($data_json, true);
+
+
+
+$genre_filter = $_GET['genre'] ?? '';
+
+// if (!empty($genre_filter)) {
+//     $filtered_discs = [];
+
+//     foreach ($discs as $disc) {
+//         $genre = $disc['genre'];
+//         if ($genre === $genre_filter) $filtered_discs[] = $disc;
+//     }
+
+//     $discs = $filtered_discs;
+// }
+// if (!empty($genre_filter)) {
+//     $filter = $genre_filter;
+//     return $filter;
+// }
+
+
+
+header('Content-Type: application/json');
+// echo $filter;
+echo json_encode($discs);
---
+++ script.js
@@ -0,0 +1,37 @@
+// console.log("VUE OK", Vue);
+
+//! VUE_JS!//
+const { createApp } = Vue;
+const apiUri = 'http://localhost/php-dischi-json/index.php?genre=';
+const app = Vue.createApp({
+    name: "Vue",
+    data() {
+        return {
+            discs: [],
+            genres: [],
+            // filter: '',
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    created() {
+        axios.get(apiUri).then(res => {
+            this.discs = res.data;
+
+
+            for (let i = 0; i < this.discs.length; i++) {
+                if (this.genres.indexOf(this.discs[i].genre) === -1) {
+                    this.genres.push(this.discs[i].genre)
+                }
+            }
+            return this.genres;
+        });
+    }
+
+});
+
+app.mount("#root");
